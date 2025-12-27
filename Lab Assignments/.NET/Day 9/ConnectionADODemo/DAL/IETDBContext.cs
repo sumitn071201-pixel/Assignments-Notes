821992d657c48b53dd396e15a9bc3ebd96f2918a
Sumit Narwade <sumitn071201@gmail.com>
2025-12-27 08:08:21 +0100

    lab assignments notes 27dec

---
+++ Lab Assignments/.NET/Day 9/ConnectionADODemo/DAL/IETDBContext.cs
@@ -0,0 +1,104 @@
+﻿using ConnectionADODemo.Models;
+using Microsoft.Data.SqlClient; // Data Source
+
+namespace ConnectionADODemo.DAL
+{
+    // Controller
+    // Business Logic
+    // Database Connectivity Layer
+    public  class IETDBContext
+    {
+        string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=IETDB;Integrated Security=True";
+
+        public List<Emp> GetEmpRecords()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Emp", connection);
+            
+            connection.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            List<Emp> allEmpRecords = new List<Emp>();
+
+            while (reader.Read())
+            {
+                allEmpRecords.Add(new Emp()
+                {
+                    EId = Convert.ToInt32(reader["Id"]),
+                    EName = reader["Name"].ToString(),
+                    EAddress = reader["Address"].ToString()
+
+                });
+            }
+            connection.Close();
+
+            return allEmpRecords;
+
+        }
+        public List<Emp> GetEmpRecordsById(int idToBeSearched)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string selectByIDQuery = $"SELECT * FROM Emp WHERE ID = {idToBeSearched}";
+
+            SqlCommand cmd = new SqlCommand(selectByIDQuery, connection);
+
+            connection.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            List<Emp> allEmpRecords = new List<Emp>();
+
+            while (reader.Read())
+            {
+                allEmpRecords.Add(new Emp()
+                {
+                    EId = Convert.ToInt32(reader["Id"]),
+                    EName = reader["Name"].ToString(),
+                    EAddress = reader["Address"].ToString()
+                });
+            }
+            connection.Close();
+
+            return allEmpRecords;
+        }
+
+        public int InsertEmpRecord(Emp emp)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            string insertQuery = $"INSERT INTO EMP(NAME, ADDRESS) VALUES ('{emp.EName}','{emp.EAddress}')";
+
+            SqlCommand cmd = new SqlCommand(insertQuery, conn);
+            conn.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            conn.Close();
+            return rowsAffected;
+        }
+        public int UpdateEmpRecord(int idToBeUpdated, Emp emptoBeUpdated)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            string updateQuery = $"UPDATE EMP SET NAME= '{emptoBeUpdated.EName}', ADDRESS = '{emptoBeUpdated.EAddress}' WHERE ID = {idToBeUpdated} ";
+
+            SqlCommand cmd = new SqlCommand(updateQuery, conn);
+            conn.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            conn.Close();
+            return rowsAffected;
+        }
+        public int DeleteEmpRecord(int idToBeDeleted)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            string deleteQuery = $"DELETE FROM EMP WHERE ID = {idToBeDeleted} ";
+
+            SqlCommand cmd = new SqlCommand(deleteQuery, conn);
+            conn.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            conn.Close();
+            return rowsAffected;
+        }
+    }
+}
---
+++ Lab Assignments/.NET/Day 9/ConnectionADODemo/Models/Emp.cs
@@ -0,0 +1,13 @@
+﻿namespace ConnectionADODemo.Models
+{
+         // Models Layer
+        // Business Entities Layer
+        // Plain CLR Object [POCO]
+        public class Emp
+        {
+            public int EId { get; set; }
+            public string? EName { get; set; }
+            public string? EAddress { get; set; }
+        }
+    
+}
